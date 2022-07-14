84f4c75c7d4621eb3c89552f63ba8682e72a1637
pandesalNaSunog <floresjem8@gmail.com>
2022-07-14 19:37:22 +0800

    latest

--- client/php/postComment.php
+++ client/php/postComment.php
@@ -4,28 +4,29 @@
     $con = connect();
     $today = getCurrentDate();
     if(isset($_SESSION['user_id']) && isset($_POST)){
-        $userId = $_SESSION['user_id'];
-        $comment = $_POST['comment'];
-        $postId = $_POST['post_id'];
+        echo $_POST;
+        // $userId = $_SESSION['user_id'];
+        // $comment = $_POST['comment'];
+        // $postId = $_POST['post_id'];
 
-        $query = "INSERT INTO comments(`user_id`,`post_id`,`comment`,`created_at`,`updated_at`)VALUES('$userId','$postId','$comment','$today','$today')";
-        $con->query($query) or die($con->error);
+        // $query = "INSERT INTO comments(`user_id`,`post_id`,`comment`,`created_at`,`updated_at`)VALUES('$userId','$postId','$comment','$today','$today')";
+        // $con->query($query) or die($con->error);
 
-        $query = "SELECT * FROM comments WHERE id = LAST_INSERT_ID()";
-        $comment = $con->query($query) or die($con->error);
-        $commentRow = $comment->fetch_assoc();
-        $commentUserId = $commentRow['user_id'];
+        // $query = "SELECT * FROM comments WHERE id = LAST_INSERT_ID()";
+        // $comment = $con->query($query) or die($con->error);
+        // $commentRow = $comment->fetch_assoc();
+        // $commentUserId = $commentRow['user_id'];
 
-        $query = "SELECT * FROM users WHERE id = '$userId'";
-        $user = $con->query($query) or die($con->error);
-        $userRow = $user->fetch_assoc();
-        $commentName = $userRow['name'];
-        $comment = $commentRow['comment'];
-        $response = array(
-            'name' => $commentName,
-            'comment' => $comment
-        );
-        echo json_encode($response);
+        // $query = "SELECT * FROM users WHERE id = '$userId'";
+        // $user = $con->query($query) or die($con->error);
+        // $userRow = $user->fetch_assoc();
+        // $commentName = $userRow['name'];
+        // $comment = $commentRow['comment'];
+        // $response = array(
+        //     'name' => $commentName,
+        //     'comment' => $comment
+        // );
+        // echo json_encode($response);
     }else{
         echo 'index.html';
     }
