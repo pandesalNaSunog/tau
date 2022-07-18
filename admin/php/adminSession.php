e46ced21984d0d79abeb29551b489afc69a4838f
pandesalNaSunog <floresjem8@gmail.com>
2022-07-18 15:30:25 +0800

    latest

--- admin/php/adminSession.php
+++ admin/php/adminSession.php
@@ -1,6 +1,11 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['admin_id'])){
-        echo 'index.html';
+    if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
+        session_start();
+        if(!isset($_SESSION['admin_id'])){
+            echo 'index.html';
+        }
+    }else{
+        echo header('HTTP/1.0 403 Forbidden');
     }
+    
 ?>
