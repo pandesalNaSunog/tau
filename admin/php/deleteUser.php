92339451b4a1b681790ec91f790cb5b01abc5da9
pandesalNaSunog <floresjem8@gmail.com>
2022-07-20 09:14:28 +0800

    latest

--- admin/php/deleteUser.php
+++ admin/php/deleteUser.php
@@ -12,6 +12,9 @@
             $query = "DELETE FROM comments WHERE user_id = '$userId'";
             $con->query($query) or die($con->error);
 
+            $query = "DELETE FROM complaints WHERE user_id = '$userId";
+            $con->query($query) or die($con->error);
+
 
             echo 'ok';
         }else{
