c6e018a68281103ed656ad602c0904e6a1327c25
pandesalNaSunog <floresjem8@gmail.com>
2022-07-21 08:33:13 +0800

    latest

--- admin/php/updateProfile.php
+++ admin/php/updateProfile.php
@@ -6,7 +6,7 @@
         if(isset($_POST) && isset($_SESSION['admin_id'])){
             $userId = $_SESSION['admin_id'];
             $email = htmlspecialchars($_POST['email']);
-            $password = password_hash(htmlspecialchars($_POST['password']));
+            $password = password_hash(htmlspecialchars($_POST['password']), PASSWORD_DEFAULT);
 
             $query = "UPDATE users SET email = '$email' AND password = '$password' WHERE id = '$userId'";
             $con->query($query) or die($con->error);
