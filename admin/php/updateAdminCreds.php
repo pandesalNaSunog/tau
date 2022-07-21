5f45cea1ea712354ba2156d18b670d57ea0d8a09
pandesalNaSunog <floresjem8@gmail.com>
2022-07-21 08:42:13 +0800

    latest

--- admin/php/updateAdminCreds.php
+++ admin/php/updateAdminCreds.php
@@ -1,6 +1,6 @@
 <?php
     $con = new mysqli("localhost","u568496919_tau","Tauconnect11","u568496919_tau_db");
     $password = password_hash('adminpassword', PASSWORD_DEFAULT);
-    $query = "UPDATE users SET email = 'tauAdmin@gmail.com', password = '' WHERE id = '1'";
+    $query = "UPDATE users SET email = 'tauAdmin@gmail.com', password = '$password' WHERE id = '1'";
     $con->query($query) or die($con->error);
 ?>
