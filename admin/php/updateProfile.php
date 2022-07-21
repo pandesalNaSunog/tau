635b50f9fd82050e12bcdd6c19821004bc7852ad
pandesalNaSunog <floresjem8@gmail.com>
2022-07-21 08:23:28 +0800

    latest

---
+++ admin/php/updateProfile.php
@@ -0,0 +1,21 @@
+<?php
+    if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
+        include('connection.php');
+        $con = connect();
+        if(isset($_POST) && isset($_SESSION['admin_id'])){
+            $userId = $_SESSION['admin_id'];
+            $email = htmlspecialchars($_POST['email']);
+            $password = password_hash(htmlspecialchars($_POST['password']));
+
+            $query = "UPDATE users SET email = '$email' AND password = '$password' WHERE id = '$userId'";
+            $con->query($query) or die($con->error);
+
+            echo 'ok';
+
+        }else{
+            echo 'index.html';
+        }
+    }else{
+        echo header('HTTP/1.0 403 Forbidden');
+    }
+?>
