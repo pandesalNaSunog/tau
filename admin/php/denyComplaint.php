527d98a24b9fad6a7b51361c9e1af16b39135e01
pandesalNaSunog <floresjem8@gmail.com>
2022-09-23 16:37:45 +0800

    latest

--- admin/php/denyComplaint.php
+++ admin/php/denyComplaint.php
@@ -2,7 +2,7 @@
     if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
         include('connection.php');
         $con = connect();
-
+        $today = getCurrentDate();
         if(isset($_POST)){
             $complaintId = $_POST['complaint_id'];
 
@@ -17,6 +17,9 @@
                 echo 'already denied';
             }else{
                 $query = "UPDATE complaints SET status = 'DENIED' WHERE id = '$complaintId'";
+                $con->query($query) or die($con->error);
+
+                $query = "INSERT INTO notifications(`user_id`,`title`,`message`,`created_at`,`updated_at`,`read`)VALUES('$complainantId','Complaint','One of you complaints has been DENIED','$today','$today','no')";
                 $con->query($query) or die($con->error);
                 echo 'ok';
             }
