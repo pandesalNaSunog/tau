cbc1fd4621db17e3a9ba90681135578dc5ffc911
pandesalNaSunog <floresjem8@gmail.com>
2022-10-03 12:18:23 +0800

    latest

--- admin/php/accountPassword.php
+++ admin/php/accountPassword.php
@@ -1,5 +1,5 @@
 <?php
-    if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) == 'XMLHttpRequest'){
+    if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
         session_start();
         include('connection.php');
         $con = connect();
